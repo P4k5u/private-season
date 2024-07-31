2bebc2970f24d5d3d15b1799b08bf60f287fff7b
paksu <paksu39@icloud.com>
2024-07-31 16:15:35 +0800

    build project

---
+++ private-screen/src/main/java/com/ps/privatescreen/PrivateScreenApplication.java
@@ -0,0 +1,13 @@
+package com.ps.privatescreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateScreenApplication.class, args);
+	}
+
+}
---
+++ private-screen/src/test/java/com/ps/privatescreen/PrivateScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.privatescreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
